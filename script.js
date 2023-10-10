35c3acc5c0299a5d7e1b0aac6ce7a930a73e4a69
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2023-10-10 23:31:48 +0100

    Add clearValues function and call it when clicking clear button on the calculator

--- script.js
+++ script.js
@@ -11,8 +11,26 @@ let operatorInput;
 let firstNumberPressed = true;
 let secondNumberPressed = false;
 
+const clearValues = () => {
+  firstNumberPressed = true;
+  secondNumberPressed = false;
+
+  firstNumberInput = undefined;
+  secondNumberInput = undefined;
+  operatorInput = undefined;
+
+  firstNumber.textContent = "";
+  secondNumber.textContent = "";
+  operator.textContent = "";
+  result.textContent = "";
+};
+
 allBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {});
+  btn.addEventListener("click", () => {
+    if (btn.textContent === "C") {
+      clearValues();
+    }
+  });
 });
 
 document.addEventListener("keypress", (event) => {});
