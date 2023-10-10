ac715d821c29bf0e4589460bff76e52f90ef2fd1
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2023-10-10 23:14:15 +0100

    Add variables in order to start coding

---
+++ script.js
@@ -0,0 +1,12 @@
+const allBtns = document.querySelectorAll("button");
+const firstNumber = document.querySelector(".user-input .firstNumber");
+const secondNumber = document.querySelector(".user-input .operator");
+const operator = document.querySelector(".user-input .secondNumber");
+const result = document.querySelector(".result");
+
+let firstNumberInput;
+let secondNumberInput;
+let operatorInput;
+
+let firstNumberPressed = true;
+let secondNumberPressed = false;
